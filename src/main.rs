b1b9359413a71f8c5f122145c65ca8d601424d25
Otto <you@example.com>
2022-05-01 16:01:12 -0700

    added faulty is_prime method

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,17 @@ use cortex_m_semihosting::{debug, hprintln};
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
     hprintln!("Hello, world!").unwrap();
+    let b_is_5_prime = is_prime(5);
+    hprintln!("is_prime(5) == {}",b_is_5_prime);
 
     debug::exit(debug::EXIT_SUCCESS);
 
     loop {
         // your code goes here
     }
+}
+
+fn is_prime(n_prime_candidate: i32) -> bool {
+    //attribution: several solutions posted here https://exercism.org/tracks/rust/exercises/nth-prime/solutions?page=1
+    !(2..(n_prime_candidate/2 + 1)).any(|n_possible_divisor| (n_prime_candidate % n_possible_divisor) == 0)
 }
